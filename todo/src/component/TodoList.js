e00664d4b4a3d490c99502a04c6dd4cdb0a3f25e
kthnaver <kth_701@naver.com>
2024-06-13 17:50:04 +0900

    - search

--- todo/src/component/TodoList.js
+++ todo/src/component/TodoList.js
@@ -1,22 +1,40 @@
 import './TodoList.css'
 import TodoItem from './TodoItem.js'
+import { useState } from "react";
 
 const TodoList = ( {todo}) => {
-  console.log(todo)
+
+  const [search, setSearch] = useState("");
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value)
+
+    console.log("search: ",search)
+  }
+  // 검색 필터 함수
+  const getSearchResult = () => {
+    return search === "" 
+              ? todo 
+              : todo.filter( (it) => it.content.toLowerCase().includes(search.toLowerCase()))
+  }
 
   return (
     <div className="TodoList">
       <h5>Todo List 🎄</h5>
       <div className="search_wrapper">
         <input className="form-control me-2" 
+               value={search}
+               onChange={onChangeSearch}
                type="Search" 
                placeholder="검색어를 입력하세요" />
       </div>
       <div className="list_wrapper">
           {/* {todo.map( (it) =>  <div>{it.content}</div>)} */}
           
-          {todo.map( (it) => <TodoItem {...it} />)}
-
+          {/* todo 할일 아이템 전체 list */}
+          {/* {todo.map( (it) => <TodoItem key={it.id} {...it} />)} */}
+          {getSearchResult().map( (it) => ( 
+                      <TodoItem key={it.id} {...it} />
+                      ))}
 
       </div>
     </div>
