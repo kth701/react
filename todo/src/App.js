554bd76a2930aaf71dff8ff6a03363345d03f3d5
kthnaver <kth_701@naver.com>
2024-06-13 16:24:15 +0900

    - 기능구현1

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,54 @@
 
 import './App.css';
+import { useState, useRef } from "react";
+
 import Header from './component/Header.js';
 import TodoEditor from './component/TodoEditor.js';
 import TodoList from './component/TodoList.js';
 
+
+// 목데이터 설정
+const mockTodo = [
+  { 
+    id:0, 
+    idDone: false, 
+    content: "React 공부하기", 
+    createdDate: new Date().getTime(),
+  },
+  { 
+    id:1, 
+    idDone: false, 
+    content: "Java 공부하기", 
+    createdDate: new Date().getTime(),
+  },
+];
+
+
 function App() {
+  // 변수(상태)
+  const [todo, setTodo] = useState(mockTodo);
+  const idRef = useRef(3); //변수 역할
+
+  // 함수(기능)
+  const onCreate = (content) => {
+    // 새 할 일 아이템 객체
+    const newItem = {
+      id: idRef.current,
+      content,
+      isDone: false,
+      createdDate: new Date().getTime()
+    }
+    //새롭게 추가된 아이템은 항상 배열의 0번 요소
+    setTodo([newItem, ...todo ])
+    idRef.current += 1;
+
+  }
+
+
+
+
+
+
   return (
     <div className="container">
       <div className="contents">
@@ -18,3 +62,20 @@ function App() {
 }
 
 export default App;
+
+
+/*
+기능 구현
+
+App     : 할 일 데이터 관리
+Header  : 오늘의 날짜 표시
+TodoEditor: 새로운 할 일 아이템 생성
+TodoList : 검색에 따라 필터링된 할 일 아이템 렌더링
+TodoItem : 할 일 아이템의 수정 및 삭제
+
+Create: 생성
+Read : 읽기
+Update : 수정
+Delete : 삭제
+
+*/
