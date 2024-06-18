04625f62f81a6988219bd9e61ad68f5b58c48653
kthnaver <kth_701@naver.com>
2024-06-18 18:21:22 +0900

    - 메모이제이션

--- todo/src/component/Header.js
+++ todo/src/component/Header.js
@@ -1,6 +1,10 @@
+import React from 'react';
 import './Header.css'
 
+
 const Header = () => {
+  console.log("Header 업데이트")
+  console.log("리렌더될 때마다 실행되는 컴포넌트입니다.");
 
   return (
     <div className="Header alert alert-warning ">
@@ -10,4 +14,14 @@ const Header = () => {
   )
 }
 
-export default Header;
+// export default Header;
+export default React.memo(Header)
+
+/*
+React.memo는 인수로 전달한 컴포넌트를 메모이제이션 컴포넌트로 만들어 반환
+React.memo가 반환하는 컴포넌트
+  : 부모 컴포넌트에서 전달된 Props가 변경되지 않는 한 리렌더되지 않음.
+  : 크고 복잡할수록 불필요한 렌더링을 방지, 브라우저의 연산량을 줄여 성능 최적화
+
+
+*/
--- todo/src/component/TodoList.js
+++ todo/src/component/TodoList.js
@@ -1,6 +1,6 @@
 import './TodoList.css'
 import TodoItem from './TodoItem.js'
-import { useState } from "react";
+import { useState , useMemo} from "react";
 
 const TodoList = ( {todo, onUpdate, onDelete}) => {
 
@@ -17,9 +17,41 @@ const TodoList = ( {todo, onUpdate, onDelete}) => {
               : todo.filter( (it) => it.content.toLowerCase().includes(search.toLowerCase()))
   }
 
+  // Todo 통계
+  // 고차 컴포넌트 : 인수로 전달된 컴포넌트를 새로운 컴포넌트로 반환하는 함수
+  // 횡단 관심사(크로스커팅관심사)
+  // : 프로그래밍에서 비즈니스 로직(핵심기능)과 구분되는 공통 기능을 지칭
+  // useMemo: 불필요한 기능은 다시 호출 하지 않도록 설정
+  // const 변수 = useMemo(콜백함수, 의존성 배열)
+  const analyzeTodo = useMemo( () => {
+    console.log("analyzeTodo 함수 호출");
+
+    const totalCount = todo.length;
+    const doneCount = todo.filter( (it) => it.isDone).length;
+    const notDoneCout = totalCount - doneCount;
+
+    return {
+      totalCount,
+      doneCount,
+      notDoneCout
+    }
+  }, // 첫번째 인자: 콜백 함수
+  [todo] // 두번째 인자: 의존성 배열:값이 변할때만 콜백함수
+);
+
+  const { totalCount, doneCount, notDoneCout } = analyzeTodo;
+
+
   return (
     <div className="TodoList">
       <h5>Todo List 🎄</h5>
+      <hr />
+      <div>
+        <div>총개수 : {totalCount}</div>
+        <div>완료된 할 일 : {doneCount}</div>
+        <div>아직 완료하지 못한 할 일 : {notDoneCout}</div>
+      </div>
+      <hr />
       <div className="search_wrapper">
         <input className="form-control me-2" 
                value={search}
